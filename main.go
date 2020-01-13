09e19db6be3cfb222f0efcff3381f09f27755adc
James Wetter <wetterj@google.com>
2020-01-13 11:54:58 +1100

    allow the port to be set by envvar

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", HelloServer)
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
-		panic(err)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
 	}
 }
 
