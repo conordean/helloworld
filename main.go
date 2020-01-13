6936661ca2ccd4bdc6c1cef1c99e879fe5d94202
James Wetter <wetterj@google.com>
2020-01-13 11:18:11 +1100

    add hello world server

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", HelloServer)
+	err := http.ListenAndServer(":80", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
